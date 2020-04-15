7ac85eb4dfc8f4532771354e73fdad23e2aba08c
Crucios <sgerrys93@gmail.com>
2020-04-15 09:31:54 +0700

    genap output

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
   name = 'Angular';
+  data = 0;
+  output = "";
+  markupPola = ""
+
+  powerCalculate(a,b){
+    this.output = "";
+    this.data = Math.pow(parseInt(a), parseInt(b));
+    this.output += this.data;
+
+    // Ganjil
+    this.markupPola = "";
+    if(this.data % 2 == 1){
+      
+    }
+    // Genap
+    else if (this.data % 2 == 0){
+      for(var i = b; i <= this.data; i++){
+        var polaBaris = "";
+        for(var j = 0; j < i; j++){
+          polaBaris += '*';
+        }
+        this.markupPola += polaBaris + "<br></br>";
+      }
+    }
+  }
+
+  clearText(){
+    document.getElementById("aText").value = "";
+    document.getElementById("bText").value = "";
+    this.output = "";
+  }
 }
